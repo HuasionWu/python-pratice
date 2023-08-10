893b89f402f927ca663234d19e8931ff418e1244
HuasionWu <1587847856@qq.com>
2023-08-10 10:30:18 +0800

    修改数据库对象

--- mysql_test.py
+++ mysql_test.py
@@ -1,9 +1,9 @@
 import pymysql
 
 # 使用 connect 方法，传入数据库地址，账号密码，数据库名就可以得到你的数据库对象
-db = pymysql.connect(host="localhost", user="root", password="123456", db="avIdol")
+db = pymysql.connect(host="localhost", user="root", password="123456", db="aaa")
 
-# 接着我们获取 cursor 来操作我们的 avIdol 这个数据库
+# 接着我们获取 cursor 来操作我们的 aaa 这个数据库
 cursor = db.cursor()
 
 sql = "insert into beautyGirls(name, age) values ('Mrs.cang', 18)"
