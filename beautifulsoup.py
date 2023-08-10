ac998fd8f55a223f2c991fd14347898b1d61576b
HuasionWu <1587847856@qq.com>
2023-08-10 10:20:16 +0800

    修改页面内容和输出注释

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -2,14 +2,14 @@ from bs4 import BeautifulSoup
 
 html_doc = """
 
-<html><head><title>学习python的正确姿势</title></head>
+<html><head><title>练习python</title></head>
 <body>
-<p class="title"><b>小帅b的故事</b></p>
+<p class="title"><b>python3.8</b></p>
 
-<p class="story">有一天，小帅b想给大家讲两个笑话
-<a href="http://example.com/1" class="sister" id="link1">一个笑话长</a>,
-<a href="http://example.com/2" class="sister" id="link2">一个笑话短</a> ,
-他问大家，想听长的还是短的？</p>
+<p class="story">今天，程序员在上班
+<a href="http://example.com/1" class="sister" id="link1">早上9点上班</a>,
+<a href="http://example.com/2" class="sister" id="link2">下午6点下班</a> ,
+你觉得这样的工作时长是否合理</p>
 
 <p class="story">...</p>
 
@@ -17,17 +17,17 @@ html_doc = """
 
 soup = BeautifulSoup(html_doc,'lxml')
 print(soup.title.string)
-#学习python的正确姿势
+#练习python
 print(soup.p.string)
-#小帅b的故事
+#python3.8
 print(soup.title.parent.name)
 #head
 print(soup.a)
-#<a class="sister" href="http://example.com/1" id="link1">一个笑话长</a>
+#<a class="sister" href="http://example.com/1" id="link1">早上9点上班</a>
 print(soup.find_all('a'))
-#[<a class="sister" href="http://example.com/1" id="link1">一个笑话长</a>, <a class="sister" href="http://example.com/2" id="link2">一个笑话短</a>]
+#[<a class="sister" href="http://example.com/1" id="link1">早上9点上班</a>, <a class="sister" href="http://example.com/2" id="link2">一个笑话短</a>]
 print(soup.find(id="link2"))
-#<a class="sister" href="http://example.com/2" id="link2">一个笑话短</a>
+#<a class="sister" href="http://example.com/2" id="link2">下午6点下班</a>
 print(soup.get_text())
 print(soup.select("title"))
 print(soup.select("body a"))
