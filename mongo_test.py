6046a05fbaee7486685be0a16410a3b620022a64
HuasionWu <1587847856@qq.com>
2023-08-10 10:29:13 +0800

    修改插入内容

--- mongo_test.py
+++ mongo_test.py
@@ -6,11 +6,11 @@ db = client.avIdol
 collection = db['girl']
 
 girl = [
-    {"name": '波多野結衣', 'bwh': '{ "b": 90, "w": 59, "h": 85}' , 'age': 30},
-    {"name": '吉泽明步', 'bwh': '{ "b": 86, "w": 58, "h": 86}' , 'age': 35},
-    {"name": '桃乃木香奈', 'bwh': '{ "b": 80, "w": 54, "h": 80}' , 'age': 22},
-    {"name": '西宫梦', 'bwh': '{ "b": 85, "w": 56, "h": 86}' , 'age': 22},
-    {"name": '松下纱荣子', 'bwh': '{ "b": 88, "w": 57, "h": 86}' , 'age': 28}
+    {"name": '刘德华', 'bwh': '{ "b": 90, "w": 59, "h": 85}' , 'age': 30},
+    {"name": '郭富城', 'bwh': '{ "b": 86, "w": 58, "h": 86}' , 'age': 35},
+    {"name": '古天乐', 'bwh': '{ "b": 80, "w": 54, "h": 80}' , 'age': 22},
+    {"name": '黎明', 'bwh': '{ "b": 85, "w": 56, "h": 86}' , 'age': 22},
+    {"name": '梁朝伟', 'bwh': '{ "b": 88, "w": 57, "h": 86}' , 'age': 28}
 ]
 result = collection.insert_many(girl)
 print(result)
